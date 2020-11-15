bb1b9e4343eab8a966bada5ed2f80ef62bd9674f
TimotheDavid <timoth.david@gmail.com>
2020-11-16 00:09:57 +0100

    make one and all request

--- src/controller/UserController.ts
+++ src/controller/UserController.ts
@@ -1,26 +1,67 @@
 import {getRepository} from "typeorm";
 import {NextFunction, Request, Response} from "express";
 import {User} from "../entity/User";
-
+import * as jwt from "jsonwebtoken"; 
 export class UserController {
 
     private userRepository = getRepository(User);
 
     async all(request: Request, response: Response, next: NextFunction) {
-        return this.userRepository.find();
+        let data = await this.userRepository.find();
+
+        if (data.length === 0) {
+            response.status(404).send({
+                "error": "not found"
+            })
+        } else {
+            response.send(data);
+        }
+
     }
 
     async one(request: Request, response: Response, next: NextFunction) {
-        return this.userRepository.findOne(request.params.id);
-    }
+        let data = request.params;
 
+        const user = await this.userRepository.find({ id: data.id });
+
+        response.send(user);
+    }
+    // create the user with this routes
     async save(request: Request, response: Response, next: NextFunction) {
-        return this.userRepository.save(request.body);
+        const data = request.body;
+
+        const token = await jwt.sign({ name: data.name, surname: data.surname }, process.env.TOKEN_SECRET)
+
+
+        const user = await {
+            name: data.name,
+            surname: data.surname,
+            access_token: token,
+            score: 0
+
+        }
+
+    await this.userRepository.save(user);
+    response.sendStatus(200);
+
     }
 
     async remove(request: Request, response: Response, next: NextFunction) {
-        let userToRemove = await this.userRepository.findOne(request.params.id);
-        await this.userRepository.remove(userToRemove);
+        // let userToRemove = await this.userRepository.findOne(request.params.id);
+        // await this.userRepository.remove(userToRemove);
+
+        let data = request.params;
+        try {
+            this.userRepository.delete({ id: data.id })
+            response.sendStatus(200);
+
+        } catch (error) {
+            response.status(500).send({
+                "error": "cannot remove user"
+            })
+        }
+
+
     }
 
 }
