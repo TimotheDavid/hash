822b9781ff6cd76ffa9ebb11c8aec7a6672a7337
TimotheDavid <timoth.david@gmail.com>
2020-11-18 15:18:37 +0100

    manage auth for login with refresh token and password

--- src/controller/AuthController.ts
+++ src/controller/AuthController.ts
@@ -3,6 +3,7 @@ import { Request, Response } from "express";
 import * as jwt from 'jsonwebtoken';
 import { User } from "../entity/User";
 import { getRepository } from "typeorm";
+import * as bcrypt  from "bcrypt";
 
 //////////////////////////////////////////////////////////
 
@@ -17,66 +18,71 @@ export class AuthController {
         // The user logs in using his last used token
         // A new token is generated after each login for security purposes
         // and is sent to user by mail (in the future)
-
-        const token = await req.body.token;
-
+        const {token, password} = await req.body;
         const data = await this.user.find({ access_token: token })
         if (data.length === 0) {
-
-
             res.status(403).send({
                 "status": "Forbidden"
             });
 
         } else {
 
+            //verify password
+            let password_verify: boolean = bcrypt.compareSync(password, data[0].password);
+
+
             // Verifying token
             jwt.verify(token, process.env.TOKEN_SECRET as string, (err: any, user: any) => {
-
-                
-
                 if (err) return res.sendStatus(403)
-
                 req.user = user
-
             })
 
+
             // Setting up new token for session
-            var access_token = await jwt.sign({ id: data[0].id }, process.env.TOKEN_SECRET, { expiresIn: '24h' });
+            var access_token = await jwt.sign({id: data[0].id}, process.env.TOKEN_SECRET, {expiresIn: '24h'});
 
-            try {
 
-                // Update token in db
-                await this.user.update(data[0].id, {
-    
-                    access_token: access_token
-    
-                })
+            if (password_verify) {
 
-                // Get user role
 
-                const userWithRole = await this.user
-                .createQueryBuilder("user")
-                .leftJoinAndSelect("user.roles", "role")
-                .where("user.id = :id", {id: data[0].id})
-                .getOne();
+                try {
 
-                // Send response with Authorization
-                res.send({
-                    "status": "success",
-                    "user": userWithRole
-                })
+                    // Update token in db
+                    await this.user.update(data[0].id, {
 
-            } catch (error) {
-                
-                console.log(error)
+                        access_token: access_token
 
+                    })
+
+                    // Get user role
+
+                    const userWithRole = await this.user
+                        .createQueryBuilder("user")
+                        .leftJoinAndSelect("user.roles", "role")
+                        .where("user.id = :id", {id: data[0].id})
+                        .getOne();
+
+                    // Send response with Authorization
+                    res.send({
+                        "status": "success",
+                        "user": userWithRole
+                    })
+
+                } catch (error) {
+
+                    console.log(error)
+
+                    res.status(403).send({
+                        "status": "Forbidden"
+                    })
+
+                }
+
+            } else {
                 res.status(403).send({
-                    "status": "Forbidden"
-                })
-
+                    "status":"Forbidden"
+                });
             }
-            
         }
 
     }
